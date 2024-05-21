b99f37accf7b54ea2a9487c0c550ce797c9a6ffe
rcslbaa <ifasmarani3125@gmail.com>
2024-05-22 01:36:31 +0800

    menambahkan class manusia public "hidup" pada header

--- compotititon/manusia.h
+++ compotititon/manusia.h
@@ -1,3 +1,14 @@
 #ifndef MANUSIA_H
 #define MANUSIA_H
 using namespace std;
+
+class manusia {
+public:
+	string name;
+	jantung varJantung;
+
+	manusia(string pName)
+		: name(pName) {
+		cout << name << " hidup\n";
+	}
+	
