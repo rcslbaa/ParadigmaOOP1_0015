9b7b1a2ef7b4c32d157d436909bbc92144d93845
rcslbaa <ifasmarani3125@gmail.com>
2024-05-22 01:41:58 +0800

    menambahkan class ibu public

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -1,3 +1,15 @@
 #ifndef IBU_H
 #define IBU_H
 #include <vector>
+
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
