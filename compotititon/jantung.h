1147a42b585d5edabdb649e2044a7151697a9da2
rcslbaa <ifasmarani3125@gmail.com>
2024-05-22 01:33:04 +0800

    menambahkan header jantung

--- compotititon/jantung.h
+++ compotititon/jantung.h
@@ -2,14 +2,3 @@
 #define JANTUNG_H
 
 using namespace std;
-
-class jantung {
-public:
-	jantung() {
-		cout << "jantung dihidupkan\n";
-	}
-	~jantung() {
-		cout << "jantung dimatikan\n";
-	}
-};
-#endif
